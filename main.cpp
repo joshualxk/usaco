6ff2684f32e2220cb9099fca36bb89ed9cdec808
joshualxk <182268141@qq.com>
2017-10-24 20:33:19 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,16 +1,23 @@
 /*
 ID: bigoldb2
 LANG: C++
-PROG: dualpal
+PROG: pprime
 */
 
-#define PROG_NAME   "dualpal"
+#define PROG_NAME   "pprime"
+
+#include <iostream>
+#include <fstream>
+#include <cstring>
+#include <ctime>
+#include <algorithm>
+#include <cmath>
 
 #ifdef __JOSHUA
 #define FILE_IN   "../test.in"
 #define FILE_OUT   "../test.out"
-#define before
-#define after   cout << "time : " << time(NULL) << endl
+#define before      time_t start_time = time(NULL)
+#define after   cout << "time : " << time(NULL) << ", cost time : " << time(NULL) - start_time << endl
 #else
 #define FILE_IN     PROG_NAME ".in"
 #define FILE_OUT    PROG_NAME ".out"
@@ -18,24 +25,17 @@ PROG: dualpal
 #define after
 #endif
 
-#include <iostream>
-#include <fstream>
-#include <cstring>
-#include <ctime>
-#include <algorithm>
-
 using namespace std;
 
-void doit();
+void solve();
 
 int main() {
     freopen(FILE_IN, "r", stdin);
     freopen(FILE_OUT, "w", stdout);
     before;
-    doit();
+    solve();
     after;
 }
 
-void doit() {
-
+void solve() {
 }
